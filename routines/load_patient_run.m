de4ea77453cdd9c8b4cc7bdd881c001f71e4bd71
MarcosOriolPago <marcos.oriol.p@gmail.com>
2026-02-27 16:30:59 +0100

    feat: initialized repo

---
+++ routines/load_patient_run.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = load_patient_channels(inputArg1,inputArg2)
+%LOAD_PATIENT_CHANNELS Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
